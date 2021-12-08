95024cd83d2f71ea5813f6f3170ecb5bfb13c462
Sushant Zope <sushantzope9096@gmail.com>
2021-12-08 22:13:15 +0530

    Added proper support for 'regex' operator
    Signed-off-by: Sushant Zope <sushantzope9096@gmail.com>

--- src/pagination/generate-pagination-filter.ts
+++ src/pagination/generate-pagination-filter.ts
@@ -7,9 +7,25 @@ import { filterDto } from './pagination.dto'
 export const generatePaginationFilter = (filter: filterDto[]) => {
     const filter_fn = {}
     for (const filterElement of filter) {
-        filter_fn[filterElement.name] = {
-            ['$' + filterElement.operator]: filterElement.value
-        }
+        if ('' + filterElement.operator === 'regex') {
+            let exp = new RegExp(filterElement.value, 'i');
+            if (filterElement.mode === 'swm') {
+                exp = new RegExp('^' + filterElement.value + '.*', 'i');
+            }
+            if (filterElement.mode === 'bnm') {
+                exp = new RegExp('.*' + filterElement.value + '.*', 'i');
+            }
+            if (filterElement.mode === 'ewm') {
+                // console.log('EWM\n');
+                exp = new RegExp(filterElement.value + '.*', 'i');
+            }
+            filter_fn[filterElement.name] = {
+                ['$' + filterElement.operator]: exp,
+            };
+        } else
+            filter_fn[filterElement.name] = {
+                ['$' + filterElement.operator]: filterElement.value,
+            };
     }
     return filter_fn
 }
