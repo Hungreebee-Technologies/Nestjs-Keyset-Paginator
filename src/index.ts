ebee45f0af4b30afb3ed3dd98deb953819e524dd
Sushant Zope <sushantzope9096@gmail.com>
2021-12-12 21:13:30 +0530

    export essential functions
    Signed-off-by: Sushant Zope <sushantzope9096@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from './paginate'
 export * from './common.types'
 export * from './pagination/generate-pagination-filter'
 export * from './pagination/generate-pagination-query'
+export * from './pagination/generate-pagination-next-key'
+export * from './pagination/generate-pagination-next-key-dto-arr'
 export * from './pagination/pagination.dto'
 import { paginate } from './paginate'
 export default paginate
